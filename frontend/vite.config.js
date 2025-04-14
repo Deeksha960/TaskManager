16a008eb22842ec3744327b3730ae4430ff9bbeb
deeksha <deeksharao960@gmail.com>
2025-04-14 20:01:28 +0530

    enter

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT || 4173,
     host: true,
+    port: 4173,
+    allowedHosts: ['taskmanager-frontend-zijb.onrender.com'], // ✅ Add your Render domain here
   },
-});
+})

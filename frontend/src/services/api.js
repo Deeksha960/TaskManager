a5a885035e6b01938abb38021b44ee883a3397ff
deeksha <deeksharao960@gmail.com>
2025-04-14 19:04:52 +0530

    enter

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskmanagerapp-jutv.onrender.com",
 });
 
 // Optional: Set auth token for protected routes
